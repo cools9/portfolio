36abec56ef92c0d130fc11fc99881b8c7ccc8144
Cools9 <rishabh4a@christacademy.in>
2026-08-07 16:56:17 +0530

    addded simple image

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use dioxus::prelude::*;
-
+static CSS: Asset = asset!("/assets/main.css");
+static ICON: Asset = asset!("/assets/pfp.jpeg");
 fn main() {
     dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
-    rsx! { "HotDog!" }
+    rsx! { 
+         document::Stylesheet { href: CSS }
+        div {  
+            class: "bg-red-500 w-64 h-64",
+            img { 
+                class: "w-64 h-64 object-cover rounded-full",
+                src:ICON
+             }
+
+        }
+     }
 }
